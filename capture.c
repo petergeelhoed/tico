dbd367ebd0d158fdab0b2c6dd04a58751ca5f686
petergeelhoed <petergeelhoed@gmail.com>
2023-01-26 09:53:37 +0100

    added sigma removal, verbose

--- capture.c
+++ capture.c
@@ -1,3 +1,4 @@
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <alsa/asoundlib.h>
@@ -10,10 +11,18 @@ int main (int argc, char *argv[])
     int mvalue = 20;
     int time = 30;
     int c;
-    while ((c = getopt (argc, argv, "b:r:m:ht:")) != -1)
+    int verbose = 0;
+    double threshold =3.;
+    while ((c = getopt (argc, argv, "b:r:m:ht:vs:")) != -1)
     {
         switch (c)
         {
+            case 'v':
+                verbose = 1;
+                break;
+            case 's':
+                threshold = atoi(optarg);
+                break;
             case 't':
                 time = atoi(optarg);
                 break;
@@ -27,7 +36,7 @@ int main (int argc, char *argv[])
                 rate = atoi(optarg);
                 break;
             case 'h':
-                fprintf (stderr, "usage:\n capture device (default default:1)\noptions:\n -m <fraction of tick to modulate and plot (default: 20)\n -b bph of the watch (default: 21600/h) \n -r sampling rate (default: 48000Hz)\n -t <measurment time> (default: 30s)"); 
+                fprintf (stderr, "usage:\n capture device (default default:1)\noptions:\n -m <fraction of tick to modulate and plot (default: 20)\n -b bph of the watch (default: 21600/h) \n -r sampling rate (default: 48000Hz)\n -t <measurment time> (default: 30s)\n -v verbose, print points to stdout\n time, tick position modulo(3600/rate), deviation, σ\n -s cutoff standarddeviation (default: 3)\n"); 
                 exit(0);
 
             default:
@@ -153,6 +162,7 @@ int main (int argc, char *argv[])
     double y = 0;
     double xx = 0;
     double xy = 0;
+    double yy = 0;
     int n = time*rate/buffer_frames;
     for (i = 0; i < n; ++i)
     {
@@ -160,19 +170,71 @@ int main (int argc, char *argv[])
         xx+=i*i;
         x+=i;
         xy+=i*maxes[i];
+        yy+=maxes[i]*maxes[i];
     }
 
     x=x*buffer_frames/rate;
     y=y/rate;
+    yy=yy/rate/rate;
     xx=xx*buffer_frames/rate*buffer_frames/rate;
     xy=xy*buffer_frames/rate/rate;
-    fprintf(stderr,"%f s/d\n",-(n*xy-x*y)/(n*xx-x*x)*86400);
-
-    for (i = 0; i < time * rate/buffer_frames; ++i)
+    double a = (y*xx-x*xy)/(n*xx-x*x);
+    double b=(n*xy-x*y)/(n*xx-x*x);
+    double s = sqrt(( yy -2*a*y-2*b*xy+2*a*b*x+a*a*n+b*b*xx)/n);
+    fprintf(stderr,"raw rate: %f s/d\n",-b*86400);
+    x = 0.;
+    y = 0.;
+    xx = 0.;
+    xy = 0.;
+    yy = 0.;
+    int m = 0;
+    if (verbose)
     {
-       // printf("%f %f\n",(double)i,(double)maxes[i]);
-        printf("%f %f\n",(double)i*buffer_frames/rate,(double)maxes[i]/rate);
+        for (i = 0; i < time * rate/buffer_frames; ++i)
+        {
+            printf("%f %f %f %f\n",
+                    (double)i*buffer_frames/rate,(double)maxes[i]/rate,
+                    ((double)maxes[i]/rate-( a+(double)i*buffer_frames/rate*b)),s);
+        }
     }
 
+    double e;
+
+    for (i = 0; i < n; ++i)
+    {
+        e = (((double)maxes[i]/rate-( a+(double)i*buffer_frames/rate*b))/s);
+        if (e*e < threshold*threshold)
+        {
+            y+=maxes[i];
+            xx+=i*i;
+            x+=i;
+            xy+=i*maxes[i];
+            yy+=maxes[i]*maxes[i];
+            m++;
+        }
+    }
+    x=x*buffer_frames/rate;
+    y=y/rate;
+    yy=yy/rate/rate;
+    xx=xx*buffer_frames/rate*buffer_frames/rate;
+    xy=xy*buffer_frames/rate/rate;
+    a = (y*xx-x*xy)/(m*xx-x*x);
+    b=(m*xy-x*y)/(m*xx-x*x);
+    s = sqrt(( yy -2*a*y-2*b*xy+2*a*b*x+a*a*m+b*b*xx)/m);
+    fprintf(stderr,"after %.1fσ removal: %.2f s/d\n",threshold,-b*86400);
+  /*  if (verbose)
+    {
+        for (i = 0; i < time * rate/buffer_frames; ++i)
+        {
+            e = (((double)maxes[i]/rate-( a+(double)i*buffer_frames/rate*b))/s);
+            if (e*e < threshold*threshold)
+            {
+            printf("%f %f %f\n",
+                    (double)i*buffer_frames/rate,(double)maxes[i]/rate,
+                    ((double)maxes[i]/rate-( a+(double)i*buffer_frames/rate*b))/s);
+            }
+        }
+    }
+*/
     exit (0);
 }

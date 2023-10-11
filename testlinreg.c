51f0dac994e78c1d10500a1c5bd6f11b4d03f9cf
petergeelhoed <petergeelhoed@gmail.com>
2023-10-11 08:56:27 +0200

    test linreg

--- testlinreg.c
+++ testlinreg.c
@@ -26,6 +26,20 @@ int main (int argc, char *argv[])
     linreg(xarr,yarr, n, &a, &b, &s);
 
     fprintf(stderr,"%d %f %f %f\n",n,a,b,s);
+    xarr[0]=1;yarr[1]=592;
+    xarr[1]=2;yarr[1]=552;
+    xarr[2]=6;yarr[2]=458;
+    xarr[3]=7;yarr[3]=120;
+    xarr[4]=10;yarr[4]=398;
+    xarr[5]=11;yarr[5]=87;
+    xarr[6]=12;yarr[6]=394;
+    xarr[7]=14;yarr[7]=358;
+    xarr[8]=16;yarr[8]=332;
+    xarr[9]=19;yarr[9]=16;
+    n = 10;
+    linreg(xarr,yarr, n, &a, &b, &s);
+
+    fprintf(stderr,"%d %f %f %f\n",n,a,b,s);
 
     exit (0);
 }

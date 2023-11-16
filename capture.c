dee017c075e4878d6112118f0a6fbfcbc3069ae3
petergeelhoed <petergeelhoed@gmail.com>
2023-11-16 14:32:07 +0100

    beaterror calulation

--- capture.c
+++ capture.c
@@ -210,9 +210,30 @@ int main (int argc, char *argv[])
 
         readShiftedBuffer(derivative, capture_handle, NN, buffer, maxp, shift, &totalshift, lowerBound, upperBound);
 
-        if (i == 10*tps)
+        if (i%(10*tps) == 0)
         {
-            fprintf(stderr, "10 seconds, starting crosscor\n");
+            int maxtick = -1;
+            int postick = 0;
+            for (int j = 0; j < NN/2; j++)
+            {
+              if ( totaltick[j] > maxtick )
+              {
+                  maxtick = totaltick[j];
+                  postick = j; 
+              }
+            }
+            int maxtock = -1;
+            int postock = NN/2;
+            for (int j = NN/2 ; j< NN ; j++)
+            {
+              if ( totaltick[j] > maxtock )
+              {
+                  maxtock = totaltick[j];
+                  postock = j; 
+              }
+            }
+            //fprintf(stderr, "10 seconds, starting crosscor\ntick = %d @%d ; tock = %d @%d\nBeaterror: %.1fms",postick,maxtick,postock,maxtock, (float)(postock-postick-8000)/48.);
+            fprintf(stderr, "Beaterror: %.1fms\n", (float)(postock-postick-8000)/48.);
         }
         if (i>10*tps)
         {

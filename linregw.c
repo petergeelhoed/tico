a3db63a7c1de7b0d74125f46ac8b4081d3786060
petergeelhoed <petergeelhoed@gmail.com>
2024-12-13 07:28:01 +0100

    linreg memfix

--- linregw.c
+++ linregw.c
@@ -5,33 +5,38 @@
 
 int main()
 {
-    unsigned int n = 100000000;
-    double* xarr = calloc(n, sizeof(int));
-    double* yvec = calloc(n, sizeof(int));
-    double* wvec = calloc(n, sizeof(int));
+    unsigned int n = 1000000;
+    double* xarr = calloc(n, sizeof(double));
+    double* yvec = calloc(n, sizeof(double));
+    double* wvec = calloc(n, sizeof(double));
 
-    unsigned int N =0;
+    if (!(xarr && yvec && wvec))
+    {
+
+        printf("Cannot allocate memory\n");
+        exit(-1);
+    }
+    unsigned int N = 0;
     double* x = xarr;
     double* y = yvec;
     double* w = wvec;
     int keepRunning = 1;
     while (keepRunning)
     {
-           keepRunning = (
-                (scanf("%lf",x) == 1) &&
-                (scanf("%lf",y) == 1) &&
-                (scanf("%lf",w) == 1) );
-           x++;
-           y++;
-           w++;
-           if (keepRunning) N++;
+        keepRunning = ((scanf("%lf", x) == 1) && (scanf("%lf", y) == 1) &&
+                       (scanf("%lf", w) == 1));
+        x++;
+        y++;
+        w++;
+        if (keepRunning)
+            N++;
     }
 
     unsigned int M = 1;
     unsigned int T = M;
 
-    double* tmp = matlinreg(xarr,N,M, yvec,wvec);
-    for (unsigned int i = 0; i < T+1; i++)
+    double* tmp = matlinreg(xarr, N, M, yvec, wvec);
+    for (unsigned int i = 0; i < T + 1; i++)
     {
         printf("%10.6g ", tmp[i]);
     }

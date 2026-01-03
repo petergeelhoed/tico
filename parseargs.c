61251acfb8281e2469d2ac1711a851d401d739ca
petergeelhoed <petergeelhoed@gmail.com>
2026-01-03 14:16:53 +0100

    arseargs clean

--- parseargs.c
+++ parseargs.c
@@ -1,12 +1,18 @@
 #include "parseargs.h"
 #include "mylib.h"
+#include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 
+#define SECS_HOUR 3600
+#define CVAL 15
+#define EVERY_WIDTH 14
 int checkFloatArg(int name, double* value, char* optarg)
 {
-    *value = atof(optarg);
-    if (*value == 0)
+
+    char* endp = NULL;
+    *value = strtod(optarg, &endp);
+    if (*value == 0 || optarg == endp)
     {
         printf("invalid float argument for -%c: '%s'\n", (char)name, optarg);
         return -1;
@@ -35,6 +41,7 @@ void parse_arguments(int argc,
                      FILE** fpInput)
 {
     int c;
+    char* endp;
     while ((c = getopt(argc, argv, "b:r:z:ht:s:e:c:m:d:w:p:f:D:v:I:lj:")) != -1)
     {
         switch (c)
@@ -44,100 +51,130 @@ void parse_arguments(int argc,
             break;
         case 'j':
             if (checkUIntArg(c, teeth, optarg) != 0)
+            {
                 exit(-1);
+            }
             break;
         case 'f':
             if (checkUIntArg(c, fitN, optarg) != 0)
+            {
                 exit(-1);
+            }
             break;
         case 'c':
             if (checkUIntArg(c, cvalue, optarg) != 0)
+            {
                 exit(-1);
-            *cvalue = *cvalue > 15 ? 15 : *cvalue;
+            }
+            *cvalue = *cvalue > CVAL ? CVAL : *cvalue;
             break;
         case 'l':
-            *everyline = 14;
+            *everyline = EVERY_WIDTH;
+
             break;
         case 'v':
             if (checkUIntArg(c, verbose, optarg) != 0)
+            {
                 exit(-1);
+            }
             break;
         case 'e':
             if (checkUIntArg(c, evalue, optarg) != 0)
+            {
                 exit(-1);
+            }
             break;
         case 'I':
             if (checkFileArg(c, fpInput, optarg, "r") != 0)
+            {
                 exit(-1);
+            }
             break;
         case 'm':
             if (checkFileArg(c, fpmaxcor, optarg, "w+") != 0)
+            {
                 exit(-1);
+            }
             break;
         case 'w':
             if (checkFileArg(c, fpposition, optarg, "w+") != 0)
+            {
                 exit(-1);
+            }
             break;
         case 'D':
             if (checkFileArg(c, fpDefPeak, optarg, "r") != 0)
+            {
                 exit(-1);
+            }
             break;
         case 'p':
             if (checkFileArg(c, fptotal, optarg, "w") != 0)
+            {
                 exit(-1);
+            }
             break;
         case 's':
-            *SDthreshold = atof(optarg);
+            endp = NULL;
+            *SDthreshold = strtod(optarg, &endp);
             if (*SDthreshold == 0.0)
             {
-                fprintf(stderr, "invalid float argument for -s '%s'\n", optarg);
+                (void)fprintf(
+                    stderr, "invalid float argument for -s '%s'\n", optarg);
                 exit(-1);
             }
             break;
         case 't':
             if (checkUIntArg(c, time, optarg) != 0)
+            {
                 exit(-1);
+            }
             break;
         case 'b':
-            if (checkUIntArg(c, bph, optarg) != 0 || *bph < 3600)
+            if (checkUIntArg(c, bph, optarg) != 0 || *bph < SECS_HOUR)
             {
-                fprintf(stderr, "refusing bph < 3600 %d\n", *bph);
+                (void)fprintf(stderr, "refusing bph < 3600 %d\n", *bph);
                 exit(-1);
             }
             break;
         case 'z':
             if (checkUIntArg(c, zoom, optarg) != 0)
+            {
                 exit(-1);
+            }
             break;
         case 'r':
             if (checkFloatArg(c, rate, optarg) != 0)
+            {
                 exit(-1);
+            }
             break;
         case 'h':
         default:
-            fprintf(stderr,
-                    "usage: capture\n"
-                    "capture reads from the microphone and timegraphs your "
-                    "mechanical watch\n"
-                    "options:\n"
-                    " -d default:2 capture device>\n"
-                    " -z 10 zoom\n"
-                    " -b 21600 bph of the watch\n"
-                    " -r 48000 sampling rate in Hz\n"
-                    " -t 30 measurement time in seconds\n"
-                    " -s 3.0 cutoff standard deviation\n"
-                    " -w <file> write positions to file\n"
-                    " -m <file> write correlation to file\n"
-                    " -p <file> write pulse to file\n"
-                    " -I <file> read from file instead of microphone\n"
-                    " -D <file> read pulse from file\n"
-                    " -c 7 cross-correlation limit\n"
-                    " -f 30 fit n points for local rate\n"
-                    " -e 4 Gaussian convolution over input\n"
-                    " -n 60 number of points to fit in local rate\n"
-                    " -l print beat error and rate on each line\n"
-                    " -j 1 number of ratched wheel teeth\n"
-                    " -v <peak> write files for this peak\n");
+            (void)fprintf(
+                stderr,
+                "usage: capture\n"
+                "capture reads from the microphone and timegraphs your "
+                "mechanical watch\n"
+                "options:\n"
+                " -d default:2 capture device>\n"
+                " -z 10 zoom\n"
+                " -b 21600 bph of the watch\n"
+                " -r 48000 sampling rate in Hz\n"
+                " -t 30 measurement time in seconds\n"
+                " -s 3.0 cutoff standard deviation\n"
+                " -w <file> write positions to file\n"
+                " -m <file> write correlation to file\n"
+                " -p <file> write pulse to file\n"
+                " -I <file> read from file instead of microphone\n"
+                " -D <file> read pulse from file\n"
+                " -c 7 cross-correlation limit\n"
+                " -f 30 fit n points for local rate\n"
+                " -e 4 Gaussian convolution over input\n"
+                " -n 60 number of points to fit in local rate\n"
+                " -l print beat error and rate on each line\n"
+                " -j 1 number of ratched wheel teeth\n"
+                " -v <peak> write files for this peak\n");
             exit(0);
             break;
         }

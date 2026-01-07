e4c4d04ffec00c2d7050171a11385c222fa307b7
petergeelhoed <petergeelhoed@gmail.com>
2026-01-07 09:49:14 +0100

    loop tests

--- testgetinput.c
+++ testgetinput.c
@@ -1,55 +1,61 @@
 #include "mylib.h"
+#include <limits.h>
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 #define BUF_SIZE 32
+#define NR_TESTS 4
+
+struct exp
+{
+    char input[BUF_SIZE];
+    double dbl;
+    int integer;
+};
 
 int main(void)
 {
-    // Part 1: Basic tests
-    char test[BUF_SIZE] = " 12 ";
-    printf("'%s': %d\n", test, getInt(test));
-    printf("'%s': %lf\n", test, getDouble(test));
+    struct exp array[NR_TESTS];
+    // NOLINTBEGIN(readability-magic-numbers)
+    array[0] = (struct exp){.input = " 34", .integer = 34, .dbl = 34};
+    array[1] = (struct exp){.input = " 34.1", .integer = 34, .dbl = 34.1};
+    array[2] = (struct exp){.input = "", .integer = INT_MIN, .dbl = NAN};
+    array[3] = (struct exp){.input = " -1.7 ", .integer = -1, .dbl = -1.7};
+    // NOLINTEND(readability-magic-numbers)
 
-    char testd[BUF_SIZE] = "12.4";
-    // Note: getInt("12.4") will return 12 and stop at the decimal point
-    printf("'%s': %d\n", testd, getInt(testd));
-    printf("'%s': %lf\n", testd, getDouble(testd));
-
-    // Part 2: Working with a file stream
-    char testl[] = "1.0\n3.1\n \n42.7\n 1";
-
-    // Open a file handle to the string testl for reading
-    FILE* fpInput = fmemopen(testl, strlen(testl), "r");
-    if (fpInput == NULL)
+    for (size_t i = 0; i < NR_TESTS; i++)
     {
-        perror("fmemopen failed");
-        return EXIT_FAILURE;
+        struct exp test1 = array[i];
+        int a = getInt(test1.input);
+        double b = getDouble(test1.input);
+        if (b != test1.dbl)
+        {
+            if (isnan(b) && isnan(test1.dbl))
+            {
+                continue;
+            }
+            printf("test %lu:\n"
+                   "input '%s' => (dbl)%lf : expected %lf\n",
+                   i,
+                   test1.input,
+                   b,
+                   test1.dbl);
+            return -1;
+        }
+        if (a != test1.integer)
+        {
+            printf("test %lu:\n"
+                   "input '%s' => (int)%d :expected %d \n",
+                   i,
+                   test1.input,
+                   a,
+                   test1.integer);
+            return -1;
+        }
     }
 
-    char* line = NULL;
-    size_t len = 0;
-
-    printf("\n--- Reading from simulated file ---\n");
-    while (getline(&line, &len, fpInput) != -1)
-    {
-        // Skip lines that are just whitespace or empty to avoid getDouble
-        // exiting
-        /*        if (line[0] == '\n' || line[0] == '\r' || line[0] == ' ')
-                {
-                    continue;
-                }
-        */
-        printf("Line Double Read: '%s' -> Value: %lf\n", line, getDouble(line));
-        printf("Line Int    Read: '%s' -> Value: %d\n", line, getInt(line));
-    }
-
-    // Cleanup
-    free(line);
-    (void)fclose(fpInput);
-
+    puts("all fine");
     return 0;
 }
-
-change the code to check for the correct doubles and ints

3ba85d6f6055485aee1ebb3d225462d3e41d3186
petergeelhoed <petergeelhoed@gmail.com>
2026-01-12 14:49:26 +0100

    rewrite ifft

--- ifft.c
+++ ifft.c
@@ -1,90 +1,101 @@
+#include <ctype.h>
 #include <errno.h>
 #include <fftw3.h>
+#include <limits.h>
+#include <locale.h> // Optional: setlocale for decimal point
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 
 #include "mydefs.h"
 #include "myfft.h"
 #include "mylib.h"
 
-int main()
+int main(void)
 {
+    if (setlocale(LC_NUMERIC, "C") == NULL)
+    {
+        (void)fprintf(stderr, "Cannot set LC_NUMERIC to C\n");
+    };
+
     unsigned int iN = INIT_N;
     unsigned int i = 0;
 
     double* tmpy = calloc(iN, sizeof(double));
     double* tmpx = calloc(iN, sizeof(double));
-    double real;
-    double img;
-    int ret = 0;
-    int index;
-    char line[LINESIZE];
-    char* endptr;
-    char* next_start;
-    while (fgets(line, sizeof(line), stdin))
+    if (!tmpx || !tmpy)
     {
-        errno = 0;
-        index = strtol(line, &endptr, DECIMAL);
-        (void)index;
+        (void)fprintf(stderr, "Memory allocation failed (initial buffers)\n");
+        free(tmpx);
+        free(tmpy);
+        return -2;
+    }
 
-        if (line == endptr)
+    double triplet[3];
+    for (;;)
+    {
+        int k = getDoublesFromStdin(3, triplet);
+        if (k < 0)
         {
-            continue; // No number found on this line
+            break;
         }
-        next_start = endptr;
-        real = strtod(next_start, &endptr);
-        if (next_start == endptr)
+        if (k < 3)
         {
-            continue; // Second number not found
-        }
-        next_start = endptr;
-        img = strtod(next_start, &endptr);
-        if (next_start == endptr)
-        {
-            continue; // Third number not found
+            continue;
         }
 
-        tmpy[i] = img;
-        tmpx[i] = real;
-
+        tmpx[i] = triplet[1];
+        tmpy[i] = triplet[2];
         i++;
+
         if (i == iN)
         {
-            iN *= 3;
-            iN /= 2;
-            double* tmp2 = realloc(tmpy, iN * sizeof(double));
-            if (tmp2)
+            unsigned int newN = (iN * 3) / 2;
+
+            double* newTmpy = realloc(tmpy, newN * sizeof(double));
+            if (!newTmpy)
             {
-                tmpy = tmp2;
-            }
-            else
-            {
-                (void)fprintf(stderr, "Memory allocation failed");
+                (void)fprintf(stderr, "Memory allocation failed (grow tmpy)\n");
+                free(tmpx);
+                free(tmpy);
                 return -2;
             }
-            tmp2 = realloc(tmpx, iN * sizeof(double));
-            if (tmp2)
+            tmpy = newTmpy;
+
+            double* newTmpx = realloc(tmpx, newN * sizeof(double));
+            if (!newTmpx)
             {
-                tmpx = tmp2;
-            }
-            else
-            {
-                (void)fprintf(stderr, "Memory allocation failed");
+                (void)fprintf(stderr, "Memory allocation failed (grow tmpx)\n");
+                free(tmpy);
+                free(tmpx);
                 return -2;
             }
+            tmpx = newTmpx;
+
+            iN = newN;
         }
     }
-    if (ret == 0)
+
+    if (i == 0)
     {
-        (void)fprintf(stderr, "Failed to parse double\n");
+        (void)fprintf(stderr, "No valid input parsed from stdin\n");
+        free(tmpy);
+        free(tmpx);
         return -1;
     }
 
     unsigned int N = i;
 
     fftw_complex* in = fftw_alloc_complex(N);
+    if (!in)
+    {
+        (void)fprintf(stderr, "fftw_alloc_complex(in) failed\n");
+        free(tmpy);
+        free(tmpx);
+        return -2;
+    }
     for (i = 0; i < N; i++)
     {
         in[i][0] = tmpx[i];
@@ -94,16 +105,30 @@ int main()
     free(tmpx);
 
     fftw_complex* out = fftw_alloc_complex(N);
+    if (!out)
+    {
+        (void)fprintf(stderr, "fftw_alloc_complex(out) failed\n");
+        fftw_free(in);
+        return -2;
+    }
 
-    /* forward Fourier transform, save the result in 'out' */
     fftw_plan p =
-        fftw_plan_dft_1d((int)(N), in, out, FFTW_BACKWARD, FFTW_ESTIMATE);
+        fftw_plan_dft_1d((int)N, in, out, FFTW_BACKWARD, FFTW_ESTIMATE);
+    if (!p)
+    {
+        (void)fprintf(stderr, "fftw_plan_dft_1d failed\n");
+        fftw_free(in);
+        fftw_free(out);
+        return -2;
+    }
+
     fftw_execute(p);
 
     for (i = 0; i < N; i++)
     {
-        printf("%d %g %g\n", i, out[i][0], out[i][1]);
+        printf("%u %g %g\n", i, out[i][0], out[i][1]);
     }
+
     fftw_destroy_plan(p);
     fftw_free(in);
     fftw_free(out);

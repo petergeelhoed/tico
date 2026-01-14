38edd6d4a7834a24e7226fc0cdafc1cce5366511
petergeelhoed <petergeelhoed@gmail.com>
2026-01-14 13:52:47 +0100

    varlength

--- 1dcor.c
+++ 1dcor.c
@@ -7,16 +7,15 @@
 #include "myfft.h"
 #include "mylib.h"
 
-
 int main()
 {
     unsigned int ArrayLength = DEFAULT_NN;
-    double d;
-    double d1;
+    double readValue0;
+    double readValue1;
     double* peak = calloc(ArrayLength, sizeof(double));
     double* peak2 = calloc(ArrayLength, sizeof(double));
 
-    unsigned int n = 0;
+    unsigned int length = 0;
     char line[LINESIZE];
     char* endptr;
     char* next_start;
@@ -27,56 +26,56 @@ int main()
         errno = 0;
 
         // 1. Convert first number
-        d = strtod(line, &endptr);
+        readValue0 = strtod(line, &endptr);
         if (line == endptr)
         {
             continue; // No number found on this line
         }
         // 2. Convert second number starting where the first one ended
         next_start = endptr;
-        d1 = strtod(next_start, &endptr);
+        readValue1 = strtod(next_start, &endptr);
         if (next_start == endptr)
         {
             continue; // Second number not found
         }
-        peak[n] = d;
-        peak2[n] = d1;
-        n++;
-        if (n == ArrayLength)
+        peak[length] = readValue0;
+        peak2[length] = readValue1;
+        length++;
+        if (length == ArrayLength)
         {
             ArrayLength = ArrayLength * 3 / 2;
-            double* p = realloc(peak, ArrayLength * sizeof(double));
-            double* q = realloc(peak2, ArrayLength * sizeof(double));
+            double* tmpArray0 = realloc(peak, ArrayLength * sizeof(double));
+            double* tmpArray1 = realloc(peak2, ArrayLength * sizeof(double));
 
-            if (p == NULL || q == NULL)
+            if (tmpArray0 == NULL || tmpArray1 == NULL)
             {
                 printf("out of memory");
             }
             else
             {
-                peak = p;
-                peak2 = q;
+                peak = tmpArray0;
+                peak2 = tmpArray1;
             }
         }
     }
-    fftw_complex* in = fftw_alloc_complex(n);
-    fftw_complex* in2 = fftw_alloc_complex(n);
-    for (unsigned int i = 0; i < n; ++i)
+    fftw_complex* in1 = fftw_alloc_complex(length);
+    fftw_complex* in2 = fftw_alloc_complex(length);
+    for (unsigned int i = 0; i < length; ++i)
     {
-        in[i][0] = peak[i];
-        in[i][1] = 0.0;
+        in1[i][0] = peak[i];
+        in1[i][1] = 0.0;
         in2[i][0] = peak2[i];
         in2[i][1] = 0.0;
     }
 
-    fftw_complex* cross = crosscor(n, in, in2);
-    for (unsigned int i = 0; i < n; i++)
+    fftw_complex* cross = crosscor(length, in1, in2);
+    for (unsigned int i = 0; i < length; i++)
     {
         printf("%d %lf %lf %lf\n", i, peak[i], peak2[i], cross[i][0]);
     }
     free(peak);
     free(peak2);
-    fftw_free(in);
+    fftw_free(in1);
     fftw_free(in2);
     fftw_free(cross);
 

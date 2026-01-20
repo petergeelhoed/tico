9a790dc3f058d966ae7c7af1714f0f83d7464804
petergeelhoed <petergeelhoed@gmail.com>
2026-01-20 08:50:39 +0100

    exit on memfailure

--- src/myarr.c
+++ src/myarr.c
@@ -1,5 +1,6 @@
 #include "myarr.h"
 #include <stdio.h>
+
 void freemyarr(struct myarr* arr)
 {
     free(arr->arr);
@@ -15,13 +16,15 @@ struct myarr* makemyarrd(unsigned int ArrayLength)
     struct myarr* ret = (struct myarr*)calloc(1, sizeof(struct myarr));
     if (ret == NULL)
     {
-        return NULL;
+        (void)fprintf(stderr, "Cannot allocate memory\n");
+        exit(EXIT_FAILURE);
     }
     ret->arrd = calloc(ArrayLength, sizeof(double));
     if (ret->arrd == NULL)
     {
         free(ret);
-        return NULL;
+        (void)fprintf(stderr, "Cannot allocate memory\n");
+        exit(EXIT_FAILURE);
     }
     ret->ArrayLength = ArrayLength;
     return ret;
@@ -31,13 +34,15 @@ struct myarr* makemyarr(unsigned int ArrayLength)
     struct myarr* ret = (struct myarr*)calloc(1, sizeof(struct myarr));
     if (ret == NULL)
     {
-        return NULL;
+        (void)fprintf(stderr, "Cannot allocate memory\n");
+        exit(EXIT_FAILURE);
     }
     ret->arr = calloc(ArrayLength, sizeof(int));
     if (ret->arr == NULL)
     {
         free(ret);
-        return NULL;
+        (void)fprintf(stderr, "Cannot allocate memory\n");
+        exit(EXIT_FAILURE);
     }
     ret->ArrayLength = ArrayLength;
     return ret;

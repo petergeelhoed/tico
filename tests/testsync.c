dde43e54ef18fcfceff40edcaa0ea3047528f591
petergeelhoed <petergeelhoed@gmail.com>
2026-01-20 11:18:37 +0100

    testsync

---
+++ tests/testsync.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "myarr.h"
+#include "mydefs.h"
+#include "mylib.h"
+#include "mysync.h"
+
+#define size 8
+int main(void)
+{
+    FILE* filePtr = fopen("testsync", "w");
+    struct myarr* test = makemyarr(size);
+    printf("%d %d %d \n", test->arr[0], test->arr[1], test->arr[2]);
+    for (int i = 0; i < 1000; i++)
+    {
+        test->arr[0] = i;
+        syncAppendMyarr(test, filePtr);
+    }
+    freemyarr(test);
+    wait();
+    (void)fclose(filePtr);
+    exit(0);
+}

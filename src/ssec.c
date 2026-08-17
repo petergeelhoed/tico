aa773c18564d95367099412db66a392b2597c6d2
petergeelhoed <petergeelhoed@gmail.com>
2026-08-17 06:44:09 +0200

    fix(ssec): remove unused macros HALF and COLOR_YELLOW

--- src/ssec.c
+++ src/ssec.c
@@ -12,9 +12,7 @@
 #define ISO_LENGTH 40
 #define LINESIZE 512
 #define VALUESIZE 512
-#define HALF 0.5
 #define COLOR_RED "\033[31m"
-#define COLOR_YELLOW "\033[34m"
 #define COLOR_GREEN "\033[32m"
 #define COLOR_RESET "\033[0m"
 
